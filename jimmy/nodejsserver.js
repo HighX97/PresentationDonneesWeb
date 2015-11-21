ca46ddfee874e72b28b059dcae5fded2ef4247a9
jimmymunoz <myappeu@gmail.com>
2015-11-21 21:00:36 +0100

    Angular Works - no Jquery

---
+++ jimmy/nodejsserver.js
@@ -0,0 +1,41 @@
+//node /Applications/XAMPP/xamppfiles/htdocs/projectpdw/jimmy/nodejsserver.js 
+// Angular debug: https://addons.mozilla.org/en-US/firefox/addon/angscope-simple-angularjs-s/
+var jimmyPath = "";
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.get('/listeMembres', function(req, res){
+	console.log("serveur Node : /listeMembres");
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Content-Type', 'application/json');
+	var redable = fs.createReadStream(jimmyPath + 'listeMembres2.json');
+	redable.on('open', function(){
+		redable.pipe(res);
+	});
+	redable.on('error', function(){
+		res.end("[]");
+	});
+});
+
+app.get('/listeMembres/:nom', function(req, res){
+	console.log("serveur Node : /listeMembres/:nom");
+	var nomACherche = req.params.nom;
+	var chaineListeMembres = fs.readFileSync(jimmyPath + 'listeMembres2.json', "UTF-8");
+	var listeMembres = JSON.parse(chaineListeMembres);
+	var listePersonnes = [];
+	for (var i = 0; i < listeMembres.length; i++){
+		var nom = listeMembres[i].nom;
+		var prenom = listeMembres[i].prenom;
+		if( listeMembres[i].nom == nomACherche ){
+			listePersonnes.push({"nom": nom, "prenom" : prenom});
+		}
+	}	
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Content-type', 'application/json');
+	var json = JSON.stringify(listePersonnes);
+	console.log(" -> json : " + json);
+	res.end(json);
+});
+
+app.listen(8888);
