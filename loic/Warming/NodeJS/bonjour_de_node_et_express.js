4032a0ce549e36131dd7c749e9ab9b05ab41f13d
jimmymunoz <myappeu@gmail.com>
2015-11-23 15:58:08 +0100

    Boostrap

--- loic/Warming/NodeJS/bonjour_de_node_et_express.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req,res) {
-	res.setHeader('Content-Type','text/plain');
-	res.end('Yaw Yaw Lowx ouW Wasav');
-});
-	
-app.listen(8889);
