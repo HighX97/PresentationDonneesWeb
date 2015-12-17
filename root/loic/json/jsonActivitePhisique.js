b9a677ed785c169d2728f343a0b930c3f184623e
jimmymunoz <myappeu@gmail.com>
2015-12-17 10:59:21 +0100

    Loic Folder

---
+++ root/loic/json/jsonActivitePhisique.js
@@ -0,0 +1,199 @@
+var objTest = (
+	{
+		"city": "Montpellier",
+		"test_data": [
+			{
+				"sport": "Taekwondo",
+				"nom": "test1",
+				"time": "1433422229",
+				"zone": "Europe/Paris",
+				"locaton": {
+					"latitude": "88.909090",
+					"longitude": "-1.909090"
+				},
+				"sportists":[
+					{
+						"name": "Jimmy Munoz",
+						"params": {
+							"genre": "male",
+							"age": 28
+						},
+						"results":[
+							"time" : {
+								"unit": "minuts",
+								"result": "2400"
+							},
+							"others": [
+								{
+									"unit": "meters",
+									"result": "100"
+								}
+							]
+						]
+					},
+					{
+						"name": "Loïc Ortolé",
+						"params": {
+							"genre": "male",
+							"age": 24
+						},
+						"results":[
+							"time" : {
+								"unit": "minuts",
+								"result": "2400"
+							},
+							"others": [
+								{
+									"unit": "meters",
+									"result": "100"
+								}
+							]
+						]
+					}
+				]
+
+			},
+			{
+				"sport": "Football",
+				"nom": "test2",
+				"time": "1433422229",
+				"zone": "Europe/Paris",
+				"locaton": {
+					"latitude": "90.909090",
+					"longitude": "-1.909090"
+				},
+				"sportists":[
+					{
+						"name": "Yu",
+						"params": {
+							"genre": "male",
+							"age": 23
+						},
+						"results":[
+							"time" : {
+								"unit": "minuts",
+								"result": "2400"
+							},
+							"others": [
+								{
+									"unit": "meters",
+									"result": "100"
+								}
+							]
+						]
+					}
+				]
+
+			},
+			{
+				"sport": "Football",
+				"nom": "test2",
+				"time": "1433422229",
+				"zone": "Europe/Paris",
+				"locaton": {
+					"latitude": "90.909090",
+					"longitude": "-1.909090"
+				},
+				"sportists":[
+					{
+						"name": "Christophe",
+						"params": {
+							"genre": "male",
+							"age": 22
+						},
+						"results":[
+							"time" : {
+								"unit": "minuts",
+								"result": "2400"
+							},
+							"others": [
+								{
+									"unit": "meters",
+									"result": "100"
+								}
+							]
+						]
+					}
+				]
+
+			}
+
+		]
+);
+console.log(objTest);
+/*
+http://www.e-s-c.fr/tests-evaluations.html
+Test de Cooper
+Test VAMEVAL 
+
+*/
+
+/*
+ l'endurance - la force - l'état de forme - la vitesse et la coordination - la souplesse - la morphologie
+*/
+
+/*
+sports aéronautiques
+alpinisme, escalade sportive
+American Football
+athlétisme
+arts martiaux
+automobilisme
+aviron
+badminton
+basketball et basketball corporatif
+bodybuilding et musculation
+boxe
+canoé-kayak
+cricket
+cyclisme
+escrime
+équitation (endurance, jumping et military voltige)
+football et football corporatif
+gymnastique
+haltérophilie
+handball
+hockey et hockey sur glace
+indiaca
+korfball
+lutte
+motocyclisme
+natation
+course d'orientation
+patinage
+plongée sous-marine
+plongeon
+powerlifting
+rugby
+skateboard
+ski
+ski nautique
+sport pour personnes présentant un handicap physique
+sport pour personnes présentant une déficience intellectuelle
+squash
+tennis
+tennis de table
+triathlon
+voile
+volleyball.
+Catégorie B : examen unique
+danse
+sports équestres (attelage, dressage, Western riding)
+tir à l'arc.
+Catégorie C : dispense de l'examen
+aéromodélisme
+billard
+sport boules
+crosse sur glace
+curling
+darts
+échecs
+golf
+golf sur piste
+jeu de quilles
+kickersport
+marche populaire
+pêche sportive
+pétanque
+tir aux armes sportives.
+*/
